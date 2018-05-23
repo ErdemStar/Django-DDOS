6f668e3a63d7ae1481d119cfc3b18535e72ea4ad
Erdem Star <erdem-yildiz@windowslive.com>
2018-05-23 09:40:44 +0300

    Kullanılan moduller

---
+++ requirements.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update
+
+sudo apt install python-pip
+sudo apt-get update
+
+sudo pip install django==1.11
+sudo apt-get update
+
+sudo pip install scapy
+sudo apt-get update
+
+sudo pip install fpdf==1.7
+sudo apt-get update
